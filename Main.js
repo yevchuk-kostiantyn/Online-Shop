0364e0f1b83a90410b472584e22a15cfe8268492
Kostiantyn <kostyantyn.yewchuk@gmail.com>
2017-12-19 14:45:18 +0200

    Add images

--- Main.js
+++ Main.js
@@ -1,13 +1,13 @@
-var json = [ {Name: "Asus PU301LA",			          Type: "Ноутбук",	  Price: 1000,  Id: "id7531", Picture: "http://www.foxtrot.com.ua/files/MediumImages/6180148/0.jpg"},
-				     {Name: "Dell Inspiron 5749",		      Type: "Ноутбук",	  Price: 21000, Id: "id7532", Picture: "http://www.laptopworld.com.pk/media/catalog/product/cache/1/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/o/p/open_box_dell_inspiron_5749_silver__1_1.jpg"},
-				     {Name: "MSI PCI-Ex GeForce GTX 960", Type: "Видеокарта", Price: 6000,  Id: "id7533", Picture: "https://dubizar.com/media/catalog/product/cache/1/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/z/o/zotac-geforce-gtx-960-amp-edition-4gb-zt-90309-10m.jpg"},
-				     {Name: "Dell UltraSharp U2412M",	    Type: "Монитор",	  Price: 7150,  Id: "id7534", Picture: "http://i.dell.com/images/global/products/root/monitor-u2413-front-std-295x295.jpg"},
-				     {Name: "A4Tech Bloody V8MA",		      Type: "Мышь",		    Price: 650,   Id: "id7535", Picture: "https://a4tech.ua/media/catalog/product/cache/14/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/v/8/v8ma_bloody_01.jpg"},
-  				   {Name: "BOSCH MSM 6B700",			      Type: "Блендер",	  Price: 1299,  Id: "id7536", Picture: "http://megakom.info/media/catalog/product/cache/1/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/5/6/562358_v01_b_2.jpg"},
-  				   {Name: "HP 255 G3",				          Type: "Ноутбук",    Price: 7100,  Id: "id7537", Picture: "http://phonetrader.ng/buy/media/catalog/product/cache/1/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/2/5/255_g3.jpg"},
-				     {Name: "Lenovo G40-30",			        Type: "Ноутбук",	  Price: 8000,  Id: "id7538", Picture: "http://phonetrader.ng/buy/media/catalog/product/cache/1/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/g/4/g40_30.jpg"},
-				     {Name: "Apple MacBook Air 13",	      Type: "Ноутбук",	  Price: 27099, Id: "id7539", Picture: "https://yellow.ua/media/catalog/product/cache/8/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/a/1/a1370_macbook_air_z0mg000cp_321233879061_1_2.jpg"}
-				    ];
+var json = [{Name: "Asus PU301LA",			      Type: "Laptop",	  Price: 1000,  Id: "id7531", Picture: "img/asus.png"},
+			{Name: "Dell Inspiron 5749",		  Type: "Laptop",	  Price: 21000, Id: "id7532", Picture: "img/dell.jpg"},
+			{Name: "MSI PCI-Ex GeForce GTX 960",  Type: "VideoCard",  Price: 6000,  Id: "id7533", Picture: "img/geforce.jpg"},
+			{Name: "Dell UltraSharp U2412M",	  Type: "Monitor",	  Price: 7150,  Id: "id7534", Picture: "img/dell2.jpg"},
+			{Name: "A4Tech Bloody V8MA",		  Type: "Mouse",	  Price: 650,   Id: "id7535", Picture: "img/a4tech.jpg"},
+  			{Name: "BOSCH MSM 6B700",			  Type: "Blender",	  Price: 1299,  Id: "id7536", Picture: "img/blender.jpg"},
+  		    {Name: "HP 255 G3",				      Type: "Laptop",     Price: 7100,  Id: "id7537", Picture: "img/hp.jpg"},
+		    {Name: "Lenovo G40-30",			      Type: "Laptop",	  Price: 8000,  Id: "id7538", Picture: "img/lenovo.jpg"},
+		    {Name: "Apple MacBook Air 13",	      Type: "Laptop",	  Price: 27099, Id: "id7539", Picture: "img/macbook.jpg"}
+		];
 
 // Decode JSON and paste it to HTML table
 function showResults () {
